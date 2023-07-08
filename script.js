09a2ee5dfc1610cc846ec84f8655d990ca7e4550
James <joshua.james918@gmail.com>
2023-07-08 15:42:41 -0500

    javascript start

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-// const  start_button = document.querySelector(".start_button button");
-// const questions_box = document.querySelector(".questions_text");
-// // const start_button = document.querySelector(".start_button button");
-// // const start_button = document.querySelector(".start_button button");
+const  start_button = document.querySelector(".start_button button");
+const questions_box = document.querySelector(".questions_text");
+
 
 // Start button clicked
-// start_button.onclick =()=>{
-//     questions_box.classList.add("")
-// }
+start_button.onclick =()=>{
+    questions_box.insert(".questions_text")
+}
 
 // creating an array and passing the number, questions, options and answers
 let questions = [
@@ -15,4 +14,3 @@ let questions = [
     }
     
 ];
-
