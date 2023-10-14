fc76dc9523c141b47f6d00a03955db97fd369bf2
James <joshua.james918@gmail.com>
2023-10-14 11:15:01 -0500

    adding questions to script1

--- assets/Scripts/script1.js
+++ assets/Scripts/script1.js
@@ -1,5 +1,9 @@
 const startButton = document.getElementById('start_btn')
 const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answers-buttons')
+
+let shuffledQuestions, currentQuestionsIndex
 
 startButton.addEventListener('click', startGame)
 
@@ -7,12 +11,33 @@ function startGame(){
     console.log(' game started')
     startButton.classList.add('hide')
     questionContainerElement.classList.remove('hide')
+    shuffledQuestions = questions.sort(() => Math.random()- .5)
+    currentQuestionsIndex = 0
+    setNextQuestion()
 }
 
 function setNextQuestion(){
+    showQuestion(shuffledQuestions[currentQuestionsIndex])
+}
 
+function showQuestion(question) {
+questionElement.innerHTML = question.question
 }
 
 function selectAnswer(){
 
 }
+
+let questions = [
+    {
+        Number: 1,
+        question: "what is JavaScript used for ?",
+        answer: "Interconnectivity",
+        options: [
+            "All the above",
+            "Interconnectivity",
+            "Markup Language",
+            "Styling the markup language",
+        ]
+    },
+    ]
