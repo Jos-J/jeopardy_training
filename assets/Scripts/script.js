9b17d628ae2178e69d47e40593a20ecaf7006421
James <joshua.james918@gmail.com>
2023-11-12 02:11:31 -0600

    questionloaded

--- assets/Scripts/script.js
+++ assets/Scripts/script.js
@@ -1,7 +1,8 @@
-// var questions = document.getElementById( 'questions')
+var questionElement = document.querySelector('.question');
+var answerButtonsElement = document.getElementById('answer-buttons')
 var correct = document.querySelector('.correct');
 var incorrect = document.querySelector('.incorrect');
-var timerElement = document.getElementsByClassName('timer-count');
+var timerElement = document.querySelector('.timer-count');
 var startButton = document.getElementById('start-btn')
 var nextButton = document.getElementById('next-btn')
 var timer;
@@ -9,17 +10,18 @@ var timerCount;
 var scoreForm
 var initialsInput;
 var timerCount = 80;
-var questionElement = document.getElementById('question')
+
 var correctCounter = 0;
 var incorrectCounter = 0;
 var isQuizFinished = false;
 var timer;
-var shuffledQuestions;
-var score = document.getElementById('answer-buttons')
+
 var answer = document.getElementById('answer')
-var currentQuestionIndex = 0
+
 var startButton 
 
+
+ // Assuming you have only one timer element
 
 // function init() {
 //     getCorrect();
@@ -28,19 +30,18 @@ var startButton
 
 startButton.addEventListener('click', startQuiz, startTimer)
 nextButton.addEventListener ('click', () => {
-    currentQuestionIndex++
-    setNextQuestion()
+    loadQuestion()
+    selectAnswer()
+    // setNextQuestion()
 })
 
 
 function startQuiz(){ 
 console.log('Quiz Started')
-// startButton.classList.add('hide')
-// shuffledQuestions = questions.sort(() => Math.random() - .5)
 isQuizFinished = false;
 currentQuestionIndex = 0
 timerCount = 1000;
-// startButton.disabled = true;
+
     startTimer()
     loadQuestion() 
     }
@@ -89,15 +90,27 @@ function startTimer () {
 function loadQuestion() {
     var randomIndex = Math.floor(Math.random() * question.length); // Corrected random index calculation
     var currentQuestion = question[randomIndex]; // Using a different variable name
-    // Rest of your code to display the currentQuestion
-    question.answer.array.forEach(element => {
-        
-    });
-    return (currentQuestion)
+    questionElement.textContent = question.currentQuestion;
+    answerButtonsElement.innerHTML = 'btn-grid'
+    currentQuestion.answer.forEach (answer => {
+        const button = document.createElement ('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        // if( answer.correct){
+        //     button.dataset.correct = answer.correct
+        // }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+    
+    // question.options.forEach(question, answer)
+   
     // ...
 }
 
+function selectAnswer(e){
 
+}
 
 // creating an array and passing the number, questions, options and answers
 var question = [
@@ -133,72 +146,72 @@ var question = [
              {text: 'yes', correct: false},
          ]
      },
-    {
-        Number: 2,
-        question: "Name some of the JavaScript features ?",
-        answer: "All the Above",
-        options: [
-            "open and cross platform",
-            "designed for creating network-centric applications",
-            "complementary to and integrated with HTML",
-            "All the above",
-        ]
-    },
-    {
-        Number: 3,
-        question: "Is syntax importamt in HTML, CSS, JavaScript?",
-        answer: "True",
-        options: [
-            "True",
-            "False",
-            "None of the above",
-            "All the above",
-        ]
-    },
-    {
-        Number: 4,
-        question: "JavaScript is the same as Java?",
-        answer: "False",
-        options: [
-            "True",
-            "False",
-            "None of the above",
-            "All the above",
-        ]
-    },
-    {
-        Number: 5,
-        question: "Is JavaScript case-sensitive?",
-        answer: "True",
-        options: [
-            "True",
-            "False",
-            "None of the above",
-            "All the above",
-        ]
-    },
-    {
-        Number: 6,
-        question: "Where is the correct place to insert the JavaScript script?",
-        answer: "Body",
-        options: [
-            "Head",
-            "Body",
-            "Footer",
-            "All the above",
-        ]
-    },
-    { Number: 7,
-        question: "Rate my quiz",
-        answer: "A worthy",
-        options: [
-            "A worthy",
-            "B worthy",
-            "C worthy",
-            "D worthy",
-            "None of the Above"
-        ]
-    }
+    // {
+    //     Number: 2,
+    //     question: "Name some of the JavaScript features ?",
+    //     answer: "All the Above",
+    //     options: [
+    //         "open and cross platform",
+    //         "designed for creating network-centric applications",
+    //         "complementary to and integrated with HTML",
+    //         "All the above",
+    //     ]
+    // },
+    // {
+    //     Number: 3,
+    //     question: "Is syntax importamt in HTML, CSS, JavaScript?",
+    //     answer: "True",
+    //     options: [
+    //         "True",
+    //         "False",
+    //         "None of the above",
+    //         "All the above",
+    //     ]
+    // },
+    // {
+    //     Number: 4,
+    //     question: "JavaScript is the same as Java?",
+    //     answer: "False",
+    //     options: [
+    //         "True",
+    //         "False",
+    //         "None of the above",
+    //         "All the above",
+    //     ]
+    // },
+    // {
+    //     Number: 5,
+    //     question: "Is JavaScript case-sensitive?",
+    //     answer: "True",
+    //     options: [
+    //         "True",
+    //         "False",
+    //         "None of the above",
+    //         "All the above",
+    //     ]
+    // },
+    // {
+    //     Number: 6,
+    //     question: "Where is the correct place to insert the JavaScript script?",
+    //     answer: "Body",
+    //     options: [
+    //         "Head",
+    //         "Body",
+    //         "Footer",
+    //         "All the above",
+    //     ]
+    // },
+    // { Number: 7,
+    //     question: "Rate my quiz",
+    //     answer: "A worthy",
+    //     options: [
+    //         "A worthy",
+    //         "B worthy",
+    //         "C worthy",
+    //         "D worthy",
+    //         "None of the Above"
+    //     ]
+    // }
 ]
 
 // function showQuestions(){
